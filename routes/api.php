0258ab538febd8f9d2553ead8423ff9c37658b97
Andremka <andresmamanikantuta@gmail.com>
2026-06-02 14:42:16 -0400

    Modificacion a la ruta api.php y al handler.php de excepciones

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,38 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ObjetivoController;
+use App\Http\Controllers\SolicitudController;
+use App\Http\Controllers\ContactoController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Rutas de autenticacion
+Route::prefix('auth')->group(function () {
+    Route::post('/registro', [AuthController::class, 'register']);
+    Route::post('/login',    [AuthController::class, 'login']);
+    Route::post('/salir',    [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/usuario',   [AuthController::class, 'user'])->middleware('auth:sanctum');
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Objetivos (publico: listar y ver)
+Route::apiResource('objetivos', ObjetivoController::class)->only(['index', 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('objetivos', ObjetivoController::class)->except(['index', 'show']);
+});
+
+// Solicitudes
+Route::post('/solicitudes', [SolicitudController::class, 'store']); // publico
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/solicitudes',                     [SolicitudController::class, 'index']);
+    Route::get('/solicitudes/{solicitud}',         [SolicitudController::class, 'show']);
+    Route::post('/solicitudes/{solicitud}/estado', [SolicitudController::class, 'cambiarEstado']);
+});
+
+// Contacto (publico: enviar mensaje)
+Route::post('/contacto', [ContactoController::class, 'store']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/contacto', [ContactoController::class, 'index']);
 });
