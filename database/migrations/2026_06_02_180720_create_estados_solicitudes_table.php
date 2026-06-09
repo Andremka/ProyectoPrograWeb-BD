4cf21ac528e60c0ab4db7d4415ffc6d84e5b8d6a
Andremka <andresmamanikantuta@gmail.com>
2026-06-09 14:26:41 -0400

    Eliminacion de default en el campo estado para realizar los triggers

--- database/migrations/2026_06_02_180720_create_estados_solicitudes_table.php
+++ database/migrations/2026_06_02_180720_create_estados_solicitudes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('estados_solicitudes', function (Blueprint $table) {
             $table->id('id_estado');
             $table->unsignedBigInteger('id_solicitud');
-            $table->string('estado', 20)->default('pendiente');
+            $table->string('estado', 20);
             $table->timestamp('fecha_cambio')->useCurrent();
             $table->text('observacion')->nullable();
 
